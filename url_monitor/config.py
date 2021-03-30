5717bdef8e8b87bb0135503d4fbc09ff4ccd43fa
Veronika Dmitrievskaya <nika.dmitrievskaya@gmail.com>
2021-03-30 10:53:33 +0300

    Add a separate .env file and a migration

--- url_monitor/config.py
+++ url_monitor/config.py
@@ -1,11 +1,24 @@
+from sqlalchemy.engine.url import URL, make_url
 from starlette.config import Config
 from starlette.datastructures import Secret
 
 config = Config('.env')
 
-DB_HOST = config('DB_HOST', default=None)
-DB_PORT = config('DB_PORT', cast=int, default=None)
-DB_USER = config('DB_USER', default=None)
-DB_PASSWORD = config('DB_PASSWORD', cast=Secret, default=None)
-DB_DSN = 'postgresql://postgres:postgres@postgres_db_container/snapshots'
-DB_ECHO = True
+DB_HOST = config("DB_HOST", default=None)
+DB_PORT = config("DB_PORT", cast=int, default=None)
+DB_USER = config("DB_USER", default=None)
+DB_PASSWORD = config("DB_PASSWORD", cast=Secret, default=None)
+DB_DATABASE = config("DB_DATABASE", default=None)
+DB_DSN = config(
+    "DB_DSN",
+    cast=make_url,
+    default=URL(
+        drivername="postgresql",
+        username=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_DATABASE,
+    ),
+)
+DB_ECHO = config("DB_ECHO", cast=bool, default=False)
