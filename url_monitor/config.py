bf8a24214ffb339a211f3cd0b9be3a26b581a7a3
Veronika Dmitrievskaya <nika.dmitrievskaya@gmail.com>
2021-03-30 10:14:56 +0300

    Add dockerfile and docker-compose

--- url_monitor/config.py
+++ url_monitor/config.py
@@ -7,5 +7,5 @@ DB_HOST = config("DB_HOST", default=None)
 DB_PORT = config("DB_PORT", cast=int, default=None)
 DB_USER = config("DB_USER", default=None)
 DB_PASSWORD = config("DB_PASSWORD", cast=Secret, default=None)
-DB_DSN = "postgresql://postgres:postgres@localhost/snapshots"
+DB_DSN = "postgresql://postgres:postgres@postgres_db_container/snapshots"
 DB_ECHO = True
